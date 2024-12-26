130e683b0ca26a21468eb8062486afc7c777f2c8
Your New Name <mahamudul408432@gmail.com>
2024-12-26 10:29:34 +0600

    add logout method to AuthController and corresponding route

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+// befor login
 Route::post('register', [AuthController::class, 'register']);
-Route::get('login', [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'login']);
+
+// after login
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
